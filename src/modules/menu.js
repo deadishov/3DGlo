cda746e30eec157f562059dde534ece728d2543f
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-28 17:23:46 +0300

    Выполнено

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,16 +1,43 @@
 const menu = () => {
-    const menuBtn = document.querySelector('.menu')
-    const menu = document.querySelector('menu')
-    const closeBtn = menu.querySelector('.close-btn')
-    const menuItems = menu.querySelectorAll('ul>li>a')
+    const menuBtn = document.querySelector('.menu');
+    const menu = document.querySelector('menu');
+    const closeBtn = menu.querySelector('.close-btn');
+    const menuItems = menu.querySelectorAll('ul>li>a');
+    const scrollBtn = document.querySelector('a[href="#service-block"]>img');
+
+    console.log(scrollBtn);
 
     const handleMenu = () => {
-        menu.classList.toggle('active-menu')
+        menu.classList.toggle('active-menu');
     }
 
-    menuBtn.addEventListener('click', handleMenu)
-    closeBtn.addEventListener('click', handleMenu)
-    menuItems.forEach((item) => item.addEventListener('click', handleMenu))
+    menuBtn.addEventListener('click', handleMenu);
+    closeBtn.addEventListener('click', handleMenu);
+    //Усложненка
+    menuItems.forEach((item) => {
+        item.addEventListener('click', (event) => {
+            handleMenu();
+            event.preventDefault();
+            const blockID = item.getAttribute('href');
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+    scrollBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        const scrollLink = document.querySelector('a[href="#service-block"]');
+        const blockID = scrollLink.getAttribute('href');
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+    //Усложненка
+
+
 };
 
 export default menu;
